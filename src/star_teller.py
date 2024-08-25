710e2f5f940ee69e848767c550e8ac3d240cc3eb
Justin Bromley <z5487611@ad.unsw.edu.au>
2024-08-25 10:39:40 +1000

    Yay

---
+++ src/star_teller.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+
+def star_sign(year: str, month: str, day: str) -> str:
+    date_str = f"{year}{month}{day}"
+
+    # See whether the date entered is valid
+    try:
+        date = datetime.strptime(date_str, "%Y%m%d")
+
+    except:
+        return "Invalid date entered"
+
+    # Covering all star signs
+    if datetime(date.year, 3, 21) <= date <= datetime(date.year, 4, 19):
+        return "Aries"
+    elif datetime(date.year, 4, 20) <= date <= datetime(date.year, 5, 20):
+        return "Taurus"
+    elif datetime(date.year, 5, 21) <= date <= datetime(date.year, 6, 20):
+        return "Gemini"
+    elif datetime(date.year, 6, 21) <= date <= datetime(date.year, 7, 22):
+        return "Cancer"
+    elif datetime(date.year, 7, 23) <= date <= datetime(date.year, 8, 22):
+        return "Leo"
+    elif datetime(date.year, 8, 23) <= date <= datetime(date.year, 9, 22):
+        return "Virgo"
+    elif datetime(date.year, 9, 23) <= date <= datetime(date.year, 10, 22):
+        return "Libra"
+    elif datetime(date.year, 10, 23) <= date <= datetime(date.year, 11, 21):
+        return "Scorpio"
+    elif datetime(date.year, 11, 22) <= date <= datetime(date.year, 12, 21):
+        return "Sagittarius"
+    elif datetime(date.year, 12, 22) <= date <= datetime(date.year, 12, 31) or datetime(date.year, 1, 1) <= date <= datetime(date.year, 1, 19):
+        return "Capricorn"
+    elif datetime(date.year, 1, 20) <= date <= datetime(date.year, 2, 18):
+        return "Aquarius"
+    elif datetime(date.year, 2, 19) <= date <= datetime(date.year, 3, 20):
+        return "Pisces"
---
+++ tests/test_star_teller.py
@@ -0,0 +1,21 @@
+import pytest
+
+from src.star_teller import star_sign
+
+class TestStarTeller:
+    def test_star_sign_working(self):
+        assert star_sign("2024", "3", "23") == "Aries"
+        assert star_sign("2024", "4", "20") == "Taurus"
+        assert star_sign("2024", "5", "21") == "Gemini"
+        assert star_sign("2024", "6", "22") == "Cancer"
+        assert star_sign("2024", "7", "24") == "Leo"
+        assert star_sign("2024", "8", "24") == "Virgo"
+        assert star_sign("2024", "10", "17") == "Libra"
+        assert star_sign("2024", "10", "25") == "Scorpio"
+        assert star_sign("2024", "12", "17") == "Sagittarius"
+        assert star_sign("2024", "12", "31") == "Capricorn"
+        assert star_sign("2024", "2", "17") == "Aquarius"
+        assert star_sign("2024", "3", "10") == "Pisces"
+
+    def test_star_sign_working_fail(self):
+        assert star_sign("234", "43", "345") ==  "Invalid date entered"
