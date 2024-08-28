06d19c06d8ecf0dab5a0f87255ee24a9f748b626
Justin Bromley <z5487611@ad.unsw.edu.au>
2024-08-28 20:46:26 +1000

    Fixed issues with pyinstaller and made an executable for the star teller app

--- main.py
+++ main.py
@@ -1,35 +1,32 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QLineEdit, QPushButton, QVBoxLayout
+import os
+from PyQt6.QtWidgets import QApplication, QWidget
 from PyQt6.QtGui import QIcon
-from PyQt6.QtCore import Qt
 from PyQt6 import uic
 
 from src.star_teller import star_sign
 
 class MyApp(QWidget):
     def __init__(self):
-        super().__init__()
-        uic.loadUi('star_sign.ui', self)
+        super(MyApp, self).__init__()
+        ui_path = os.path.join(os.path.dirname(__file__), './ui/star_sign.ui')
+        uic.loadUi(ui_path, self)
         self.setWindowTitle("Star Teller")
 
         self.button_get_star_sign.clicked.connect(self.get_star_sign)
-        
+
     def get_star_sign(self):
         day = self.line_day.text()
         month = self.line_month.text()
         year = self.line_year.text()
-
-        print(f"Day: {day}")
-        print(f"Month: {month}")
-        print(f"Year: {year}")
-
         result = star_sign(year, month, day)
-        print(f"This is the {result}")
-
         self.label_star_sign.setText(result)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    icon_path = os.path.join(os.path.dirname(__file__), './resources/star.png')
+    app.setWindowIcon(QIcon(icon_path))
     my_app = MyApp()
     my_app.show()
+
     sys.exit(app.exec())
